6fa302500571aa77486d4346e5c05e6b6d779f1a
Patrick Scholefield <paddys543@gmail.com>
2025-10-30 21:47:36 +1100

    worked on python script

--- nextMetro.py
+++ nextMetro.py
@@ -30,24 +30,30 @@ def getSchedule():
     elif day == 6:
         return sunday
 
-def getNextMetro():
-    hour = getTime().hour
-    minute = getTime().minute
-    schedule = getSchedule()
+def getNextMetro(hour, minute, schedule):
     
-    for i in range(len(schedule)):
-        if hour > schedule[i].hour or hour == schedule[i].hour and minute > schedule[i].minute:
-            if hour < schedule[i+1].hour or hour == schedule[i+1].hour and minute < schedule[i+1].minute:
-                metroHour = schedule[i].hour
-                metroMinute = schedule[i].minute
-                while metroHour < hour or metroMinute < minute:
-                    metroMinute += schedule[i].freq
-                    if metroMinute >= 60:
-                        metroMinute -= 60
-                        metroHour += 1
-    
+    if hour < schedule[0].hour or hour > schedule[len(schedule)-1].hour:
+        nextMetro = [schedule[0].hour, schedule[0].minute]
+    else:
+        for i in range(len(schedule)):
+            if hour > schedule[i].hour or hour == schedule[i].hour and minute > schedule[i].minute:
+                if hour < schedule[i+1].hour or hour == schedule[i+1].hour and minute < schedule[i+1].minute:
+                    nextMetro = findNextMetro(hour, minute, schedule[i])
+
+    return nextMetro
+
+
+def findNextMetro(hour, minute, timetable):
+    metroHour = timetable.hour
+    metroMinute = timetable.minute
+    while metroHour < hour or metroMinute < minute:
+        metroMinute += timetable.freq
+        if metroMinute >= 60:
+            metroMinute -= 60
+            metroHour += 1
+
     return [metroHour, metroMinute]
 
 time = [getTime().hour, getTime().minute]
 
-print(str(getNextMetro()[1]-time[1]) + " mins")
+print(str(getNextMetro(2, 30, getSchedule())[1]-30) + " mins")
